51daecfeea838a6c7e428b1e3b8edf57e6b2a037
Parth Chaturvedi <parthchaturvedi15@gmail.com>
2026-02-03 13:00:19 +0530

    Configure Tailwind CSS v4 with Vite + React

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,11 @@
+function HomePage(){
+    return(
+        <>
+        <div>
+            <h1>Welcome to Gobi-Store</h1>
+            <button>Login</button><p>Already Have An Account? click here to go to login</p>
+        </div>
+        </>
+    )
+}
+export default HomePage;
